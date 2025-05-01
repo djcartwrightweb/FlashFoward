03fc5e182ea13580998df2cab8c7b89dda7740c7
DJ Cartwright <dj.cartwright.web@gmail.com>
2025-05-01 18:16:51 -0400

    Initial Commit

---
+++ FlashForward/FlashForwardApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashForwardApp.swift
+//  FlashForward
+//
+//  Created by David Cartwright on 2025-05-01.
+//
+
+import SwiftUI
+
+@main
+struct FlashForwardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
